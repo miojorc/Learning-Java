07c4811a676baa6e6e139b9e08cceeac8cf9c3ef
miojorc <91229107+miojorc@users.noreply.github.com>
2024-10-06 20:31:54 -0300

    grafic of fibonacci fix

--- Interface.java
+++ Interface.java
@@ -80,26 +80,36 @@ public class Interface {
         for (int i = 0; i<B; i++){ //concertar "centro"
           if(i%4 == 1) {
             g.setColor(Color.RED);
-            angle = 90;
           }
           else if(i%4 == 2) {
             g.setColor(Color.BLUE);
+          }
+          else if(i%4 == 3){
+            g.setColor(Color.YELLOW);
+          }else{
+            g.setColor(Color.GREEN);
+          }
+          g.fillRect(config[i][0]+W, config[i][1]+H, config[i][2], config[i][2]);
+        }
+        g.setColor(Color.BLACK);
+        for (int i = 0; i<B; i++){
+          if(i%4 == 1) {
+            angle = 90;
+          }
+          else if(i%4 == 2) {
             if(i!=0) c1 = -config[i][2];
             angle = 0;
           }
           else if(i%4 == 3){
-            g.setColor(Color.YELLOW);
             if(i!=0) c1 = -config[i][2];
             if(i!=0) c2 = -config[i][2];
             angle = 270;
           }else{
-            g.setColor(Color.GREEN);
             c2 = -config[i][2];
             angle = 180;
           }
-          g.fillRect(config[i][0]+W, config[i][1]+H, config[i][2], config[i][2]);
           g.setColor(Color.BLACK);
-          g.drawArc(config[i][0]+W+c1, config[i][1]+H+c2, config[i][2]*2, config[i][2]*2, angle, 95);
+          g.drawArc(config[i][0]+W+c1, config[i][1]+H+c2, config[i][2]*2, config[i][2]*2, angle, 90);
           c1 = 0;
           c2 = 0;
         }
