988979e82ff75b0091ce87a43c21c3edabdd8bde
miojorc <91229107+miojorc@users.noreply.github.com>
2024-10-14 12:33:31 -0300

    exclude unecessari imports
    I forgot to exclude this import

--- Inicializer.java
+++ Inicializer.java
@@ -4,8 +4,6 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import javafx.event.ActionEvent;
-
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
