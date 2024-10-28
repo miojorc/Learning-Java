29ff9f227abbb29b7a1ae89fe8fa1b84ae110ff6
miojorc <91229107+miojorc@users.noreply.github.com>
2024-10-28 19:27:01 -0300

    pre merge

--- Inicializer.java
+++
@@ -1,9 +0,0 @@
-public class Inicializer {
-  public Inicializer(){
-    
-  }
-
-  public static void main(String[] args){
-    new AlienGameInterface();
-  }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Atari-Alien_Attack
-
-The classic Alien Invasion game
-states: shots made, enemies and barrier in progress
